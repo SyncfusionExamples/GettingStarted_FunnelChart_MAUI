8145abc06cbe18746c4277a0e79277e56160fc90
Kamalesh-Periyasamy <Kamalesh.periyasamy@syncfusion.com>
2026-03-26 15:48:10 +0530

    Added new sample

---
+++ Chart_GettingStarted/Chart_GettingStarted/AdmissionViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chart_GettingStarted
+{
+    public class AdmissionViewModel
+    {
+        public List<AdmissionModel> Data { get; set; }
+
+        public AdmissionViewModel()
+        {
+            Data = new List<AdmissionModel>()
+            {
+                new AdmissionModel() {XValue = "Enrolled", YValue=175},
+                new AdmissionModel() {XValue = "Admits", YValue=190},
+                new AdmissionModel() {XValue = "Applicants", YValue=245},
+                new AdmissionModel() {XValue = "Inquiries ", YValue=290},
+                new AdmissionModel() {XValue = "Prospects ", YValue=320},
+            };
+        }
+    }
+}
---
+++ Chart_GettingStarted/Chart_GettingStarted/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Chart_GettingStarted;
+
+public partial class MainPage : ContentPage
+{
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
+
